580cda094f13e02159c26437df1fdc06954507c4
omarrabbani <omarrabbani87@gmail.com>
2025-01-01 15:02:34 -0500

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import ProfileImage from './ProfileImage.png'
 import UploadArea from './UploadArea.png'
 import Parcelicon from './Parcelicon.png'
 
+export const url = 'https://fruit-cravings-backend.onrender.com'
+export const currency = '$'
+
 export const assets = {
     FruitLogo,
     AddIcon,
