7e0db3266222b082837034930d5b4b0abca0abb7
omarrabbani <omarrabbani87@gmail.com>
2025-01-01 15:00:53 -0500

    Update StoredContext.jsx

--- frontend/src/context/StoredContext.jsx
+++ frontend/src/context/StoredContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 const StoreProvider = (props) => {
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://fruit-cravings-backend.onrender.com";
     const [token,setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
